893e0fd420c3e920e57d11aa2cacf98661814615
Ahuramal <a.azimi95121@gmail.com>
2022-12-13 16:34:19 +0430

    Loading data into database

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sqlalchemy as db
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    engine = db.create_engine(f"mysql+pymysql://{'admin'}:{'azimi#1234'}@localhost/assignment")
+    # connection = engine.connect()
+    engine.connect()
+
+    training_data = pd.read_csv('datasets/train.csv')
+    training_data.to_sql('training_data3', engine, index=False)
+
+    ideal_functions = pd.read_csv('datasets/ideal.csv')
+    ideal_functions.to_sql('ideal_functions', engine, index=False)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
